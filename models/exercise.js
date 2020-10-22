38a98aa0913e86b1c4b73852f96f90c5a763c2c5
Andrew0502 <agcmarcus@gmail.com>
2020-10-21 20:58:06 -0400

    Built out Exercise Schema with help from tutor.

--- models/exercise.js
+++ models/exercise.js
@@ -2,9 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-  placeholder: {
-    
+  day: {
+    type: {
+        type: Date,
+        default: () =>  {
+            new Date()
+        },
   },
+},
+  exercises: [
+      {
+          type: {
+              type: String,
+              trim: true,
+              required: "Type is required!",
+          },
+          name: {
+              name: String,
+              trim: true,
+              required: "Name is required!",
+          },
+          duration: {
+            duration: number,
+            trim: true,
+            required: "Duration is required!",
+        },
+          weight: Number,
+          reps: Number,
+          sets: Number,
+          distance: Number,
+      },
+],
 
 });
 
